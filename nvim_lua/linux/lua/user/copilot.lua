8e26e09340b63c792d0f767c49a8741204870cdc
Claudiu Leoveanu <leoveac@amazon.com>
2023-01-10 09:54:24 +0200

    linux setup

---
+++ nvim_lua/linux/lua/user/copilot.lua
@@ -0,0 +1,34 @@
+-- use this table to disable/enable specific filetypes
+-- vim.g.copilot_filetypes = { xml = false }
+
+-- since most are enabled by default you can turn them off
+-- using this table and only enable for a few filetypes
+vim.g.copilot_filetypes = {
+    ["*"]=false,
+    python=true,
+    markdown=true,
+    html=true,
+    css=true,
+    lua=true
+}
+
+
+vim.cmd[[imap <silent><script><expr> <leader><CR> copilot#Accept("\<CR>")]]
+vim.keymap.set('i', '<leader>]', '<Plug>(copilot-dismiss)')
+
+vim.g.copilot_no_tab_map = true
+
+-- <C-]>                   Dismiss the current suggestion.
+-- <Plug>(copilot-dismiss)
+--
+--                                                 *copilot-i_ALT-]*
+-- <M-]>                   Cycle to the next suggestion, if one is available.
+-- <Plug>(copilot-next)
+--
+--                                                 *copilot-i_ALT-[*
+-- <M-[>                   Cycle to the previous suggestion.
+-- <Plug>(copilot-previous)
+
+
+vim.cmd[[highlight CopilotSuggestion guifg=#555555 ctermfg=8]]
+
--- nvim_lua/linux/lua/user/plugins.lua
+++ nvim_lua/linux/lua/user/plugins.lua
@@ -116,7 +116,8 @@ return packer.startup(function(use)
         'ellisonleao/gruvbox.nvim'
     }
 
-	-- Cmp
+	-- LSP & completion
+    use { "github/copilot.vim" }
     use {
         "hrsh7th/nvim-cmp",
         commit = "b0dff0ec4f2748626aae13f011d1a47071fe9abc"
@@ -148,16 +149,6 @@ return packer.startup(function(use)
         run = 'cd app && npm install',
         cmd = 'MarkdownPreview'
     }
-
-	-- Snippets
-    use {
-        "L3MON4D3/LuaSnip",
-        commit = "8f8d493e7836f2697df878ef9c128337cbf2bb84"
-    } --snippet engine
-    use {
-        "rafamadriz/friendly-snippets",
-        commit = "2be79d8a9b03d4175ba6b3d14b082680de1b31b1"
-    } -- a bunch of snippets to use
 
 	-- LSP
 	use {
@@ -197,6 +188,10 @@ return packer.startup(function(use)
 	use {
         "lewis6991/gitsigns.nvim",
         commit = "2c6f96dda47e55fa07052ce2e2141e8367cbaaf2"
+    }
+
+    use {
+        ("junegunn/vim-easy-align")
     }
 
 	-- Automatically set up your configuration after cloning packer.nvim
