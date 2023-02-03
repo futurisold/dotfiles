c55283d628d7103dc7b315590a33c149c4f187c0
leoentersthevoid <c.leov@icloud.com>
2023-02-03 11:18:57 +0200

    mac update (see linux commit)

--- nvim_lua/mac/lua/user/colorscheme.lua
+++ nvim_lua/mac/lua/user/colorscheme.lua
@@ -1,6 +1,51 @@
-local colorscheme = "tokyonight"
+local colorscheme = "rose-pine"
+
+if colorscheme == "rose-pine" then
+    require('rose-pine').setup({
+        --- @usage 'main' | 'moon'
+        dark_variant = 'moon',
+        bold_vert_split = false,
+        dim_nc_background = false,
+        disable_background = false,
+        disable_float_background = false,
+        disable_italics = true,
+
+        --- @usage string hex value or named color from rosepinetheme.com/palette
+        groups = {
+            background = 'base',
+            panel = 'surface',
+            border = 'highlight_med',
+            comment = 'muted',
+            link = 'iris',
+            punctuation = 'subtle',
+
+            error = 'love',
+            hint = 'iris',
+            info = 'foam',
+            warn = 'gold',
+
+            headings = {
+                h1 = 'iris',
+                h2 = 'foam',
+                h3 = 'rose',
+                h4 = 'gold',
+                h5 = 'pine',
+                h6 = 'foam',
+            }
+            -- or set all headings at once
+            -- headings = 'subtle'
+        },
+
+        -- Change specific vim highlight groups
+        highlight_groups = {
+            ColorColumn = { bg = 'rose' },
+            Comment = { fg = 'muted', style = 'italic' },
+        }
+    })
+end
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
   return
 end
+
