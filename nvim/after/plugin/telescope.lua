dcbb1f5cb3905b28029c5ec2b09fa9f2415c8fcc
futurisold <futurisold@gmail.com>
2026-03-23 19:12:22 +0200

    (fix) disable treesitter in previewer to avoid ts_highlighter crash

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -10,6 +10,7 @@ telescope.setup {
         prompt_prefix = " ",
         selection_caret = " ",
         path_display = { "smart" },
+        preview = { treesitter = false },
         mappings = {
             i = {
                 ["<C-n>"] = actions.cycle_history_next,
